c55458b12a958ffddd9ed595102c9965b8fc6b20
Omniverse <omniverse@0101.local>
2026-02-20 15:54:15 +0545

    sotre nove backend

---
+++ src/api/information-to-collect/controllers/information-to-collect.ts
@@ -0,0 +1,7 @@
+/**
+ * information-to-collect controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::information-to-collect.information-to-collect');
---
+++ src/api/information-to-collect/services/information-to-collect.ts
@@ -0,0 +1,7 @@
+/**
+ * information-to-collect service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::information-to-collect.information-to-collect');
